2d622e5088b310cf2f7f060bf159f97522ff68e9
Mahliyo <gasandigital13@gmail.com>
2026-04-09 10:14:50 +0500

    main.jsga o'tdim json

--- main.js
+++ main.js
@@ -1,5 +1,74 @@
 let container = document.getElementById("container");
 let searchInput = document.getElementById("searchInput");
  let Uzbek = [
-    
+    {
+    "id": 1,
+    "nomi": "Mahallada duv-duv gap",
+    "janri": "Komediya",
+    "yili": 1960,
+    "rejissyor": "Yo'ldosh A'zamov"
+  },
+  {
+    "id": 2,
+    "nomi": "O'tgan kunlar",
+    "janri": "Tarixiy drama",
+    "yili": 1969,
+    "rejissyor": "Yo'ldosh A'zamov"
+  },
+  {
+    "id": 3,
+    "nomi": "Abdullajon",
+    "janri": "Fantastik komediya",
+    "yili": 1991,
+    "rejissyor": "Zulfiqor Musoqov"
+  },
+  {
+    "id": 4,
+    "nomi": "Osmondagi bolalar",
+    "janri": "Drama",
+    "yili": 2002,
+    "rejissyor": "Zulfiqor Musoqov"
+  },
+  {
+    "id": 5,
+    "nomi": "Vatan",
+    "janri": "Harbiy drama",
+    "yili": 2006,
+    "rejissyor": "Zulfiqor Musoqov"
+  },
+  {
+    "id": 6,
+    "nomi": "Parizod",
+    "janri": "Drama",
+    "yili": 2012,
+    "rejissyor": "Ayub Shahobiddinov"
+  },
+  {
+    "id": 7,
+    "nomi": "Ilhaq",
+    "janri": "Tarixiy drama",
+    "yili": 2020,
+    "rejissyor": "Jahongir Ahmedov"
+  },
+  {
+    "id": 8,
+    "nomi": "Faridaning ikki ming qo'shig'i",
+    "janri": "Drama",
+    "yili": 2020,
+    "rejissyor": "Yolqin To'ychiyev"
+  },
+  {
+    "id": 9,
+    "nomi": "Bahodir Yalangto'sh",
+    "janri": "Tarixiy, biografik",
+    "yili": 2024,
+    "rejissyor": "Jahongir Ahmedov"
+  },
+  {
+    "id": 10,
+    "nomi": "Daydi shamol",
+    "janri": "Kiber-triller",
+    "yili": 2025,
+    "rejissyor": "Sanjar Matkarimov"
+  }
  ]
